a7c54a2ff3344860cc23f49dfbfe2d9d805d4039
Sathishkumar Thangavel <sathish.thangavel@solitontech.local>
2020-07-12 15:56:54 +0530

    Add : Testimonial card
    Signed-off-by: Sathishkumar Thangavel <sathish.thangavel@solitontech.local>

--- scripts/webcomponents/nextroTestimonial.js
+++ scripts/webcomponents/nextroTestimonial.js
@@ -63,21 +63,20 @@ class NextroTestimonialCard extends HTMLElement {
             font-size: 50px;
             color:#4f4f4f;
             vertical-align: -.4em;
-        }   
-        .quotes{
-            font-size:80px;
-            color: #4f4f4f;
         }
-        .rq{
+        .rating-con{
+            display:flex;
+            justify-content:flex-end;
             position:relative;
-            
+            right:50px;
+            bottom:15px;
         }
         /*Media queries*/
         @media only screen and (max-width: 700px) {
           
         }
         </style>
-        <div class="testimonial-con flex-row perfect-cen">
+        <div class="testimonial-con">
           <div class="testi-holder flex-row align-cen">
              <div class="image-wrapper">
                 <img class="face" src="/assets/face/sample-face.jpg">
@@ -86,23 +85,40 @@ class NextroTestimonialCard extends HTMLElement {
                 <span id="card-description" class="card-description"></span>
              </div>
           </div>
+          <div id="rating-box" class="rating-con">
+
+          </div>
         </div>
         `;
-
         this.cardDescription =  this.shadowRoot.querySelector('#card-description');
+        this.ratingBox =  this.shadowRoot.querySelector('#rating-box');
     }
     connectedCallback() {  }
 
     static get observedAttributes() {
-        return ['img-src','card-description'];
+        return ['img-src','card-description','rating'];
     }
     handleDescriptionText(val){
         this.cardDescription.innerText = val;
+    }
+    handleRating(val){
+        let viewTemplate='';
+        let template = `<div style="margin-right:5px;"><svg width="25" height="25" viewBox="0 0 512 512"><path d="M480,208H308L256,48,204,208H32l140,96L118,464,256,364,394,464,340,304Z" style="fill: #0747a6;stroke: #757575;stroke-linejoin:round;stroke-width: 10px;"/></svg></div>`;
+        for(let i = 0; i < Number(val) ; i++)
+        {
+            viewTemplate +=template; 
+        }
+        viewTemplate = '<div style="color:#4f4f4f;font-weight:bold;">Rating &nbsp&nbsp;&nbsp; </div>'+viewTemplate;
+        this.ratingBox.innerHTML = viewTemplate;
     }
     attributeChangedCallback(attrName,oldVal,newVal) {
         if(attrName === 'card-description')
         {
             this.handleDescriptionText(newVal);
+        }
+        if(attrName === 'rating')
+        {
+            this.handleRating(newVal);
         }
     }
     disconnectedCallback() {  }
